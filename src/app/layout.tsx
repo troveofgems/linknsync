f8d83441007d92bdca83e2960e18d2870c7981e5
Dustin K Greco <dkgreco@thetroveofgems.tech>
2026-01-26 12:11:10 -0700

    Added Vercel Meta Tags for OpenGraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,22 @@ export default function RootLayout({
             },
         }}>
             <html lang="en" suppressHydrationWarning>
+            <head>
+                <title>Link-N-Sync Service</title>
+                <meta property="twitter:image" content="Twitter link preview image URL"/>
+                <meta property="twitter:card" content="summary_large_image"/>
+                <meta property="twitter:title" content="Link-N-Sync Service"/>
+                <meta property="twitter:description"
+                      content="ICal Consolidation for Short Term Vacation Rental Property Managers"/>
+                <meta property="description"
+                      content="Link-N-Sync, the ICal Consolidation for Short Term Vacation Rental Property Managers."/>
+                <meta property="og:image" content="Link preview image URL"/>
+                <meta property="og:site_name" content="Link-N-Sync Service"/>
+                <meta property="og:title" content="Link-N-Sync Service"/>
+                <meta property="og:description"
+                      content="Link-N-Sync, the ICal Consolidation for Short Term Vacation Rental Property Managers."/>
+                <meta property="og:url" content="https://linknsync-yvxh.vercel.app"/>
+            </head>
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
