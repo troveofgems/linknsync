44f864bfb16f325db6df17370d91226e2ced1195
Dustin K Greco <dkgreco@thetroveofgems.tech>
2026-02-12 21:15:49 -0700

    Prepping Payments Page

--- src/app/(protected-pages)/(app-users)/dashboard/payments/page.tsx
+++ src/app/(protected-pages)/(app-users)/dashboard/payments/page.tsx
@@ -1,17 +1,17 @@
 "use client";
 import {SessionDataState, useUserStore} from "@/store/userStore";
 import {LoaderSkeleton} from "@/components/structural/loader/Custom.Loader";
-import {useSearchParams} from "next/navigation";
+/*import {useSearchParams} from "next/navigation";*/
 
 function PaymentsPage() {
-    const
+/*    const
         { user: { isLoading, attrs } } = useUserStore(),
-        searchParams = useSearchParams();
+        searchParams = useSearchParams();*/
 
     return (
         <div>
             <h2>Services Subscription</h2>
-            {isLoading && <LoaderSkeleton loadingMessage={"Loading Payments Screen"} additionalClassNames={""} />}
+            {/*{isLoading && <LoaderSkeleton loadingMessage={"Loading Payments Screen"} additionalClassNames={""} />}*/}
             {/*{!isLoading && <ProfileForm userProfile={attrs as SessionDataState} redirectFromOrgRegistration={(searchParams?.get("registeredOrg") as unknown as boolean || false)} /> }*/}
         </div>
     );
